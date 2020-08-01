0c094381b595047e8942de1291be3d00c44d8222
juanitatc <66490549+juanitatc@users.noreply.github.com>
2020-07-31 22:44:07 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <table>
+<thead>
+<tr>
+<th>NOMBRES</th>
+<th>APELLIDOS</th>
+<th>CORREO</th>
+<th>CELULAR</th>
+  </tr>
+  </thead>
+<tbody>
+  <tr>
+  <td>Manuela</td>
+<td>Ospina gallego</td>
+<td>manuelaospinagallego.gmail.com</td>
+<td>3209384225</td>
+
+</tr>
+<tr>
+<td>Juanita</td>
+<td>Tejada calderón</td>
+<td>juanitatejadacalderon.gmail.com</td>
+<td>3206349547</td>
+</tr>
+<tr>
+
+</tr>
+</tbody>
+
+
+        </table>
+
+    </div>
+  );
+}
+
+
